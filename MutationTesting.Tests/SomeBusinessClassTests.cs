c16a1b2678fe1645819dfa0a4e987324b75fc96a
Larry Gasik <larry@lgproductions.us>
2025-06-23 11:26:26 -0500

    Wrote some of the basics of unit tests, includign bad ones to manipulate code coverage

---
+++ MutationTesting.Tests/SomeBusinessClassTests.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Moq;
+
+namespace MutationTesting.Tests
+{
+    [TestClass]
+    public class SomeBusinessClassTests
+    {
+        private SomeBusinessClass _sut;
+        private Mock<ISomeInterface> _someInterfaceMock;
+
+        [TestInitialize]
+        public void Initialize()
+        {
+            _someInterfaceMock = new Mock<ISomeInterface>();
+            _someInterfaceMock.Setup(x => x.SomeCallAsync(It.IsAny<string>()))
+                .Returns(Task.CompletedTask);
+            _someInterfaceMock.Setup(x => x.SomeCallAsync("error")).Throws<InvalidDataException>();
+            _sut = new SomeBusinessClass(_someInterfaceMock.Object);
+        }
+
+        [TestMethod]
+        public void SomeMethod_DumbUnitTest()
+        {
+            Assert.AreNotEqual(0, _sut.SomeMethod(2, 3));
+        }
+
+        [TestMethod]
+        public async Task SomeOtherMethod_TransformsToUpperAndCallsInterface()
+        {
+            string input = "test";
+            string result = await _sut.SomeOtherMethod(input);
+            Assert.AreEqual("TEST", result);
+        }
+    }
+}
---
+++ MutationTesting/SomeBusinessClass.cs
@@ -0,0 +1,37 @@
+﻿namespace MutationTesting
+{
+    public class SomeBusinessClass
+    {
+        private ISomeInterface _someInterface;
+        public SomeBusinessClass(ISomeInterface someInterface)
+        {
+            _someInterface = someInterface ?? throw new ArgumentNullException(nameof(someInterface));
+        }
+        public int SomeMethod(int x, int y)
+        {
+            return x + y;
+        }
+        
+        public async Task<string> SomeOtherMethod(string input)
+        {
+            if (string.IsNullOrEmpty(input))
+            {
+                throw new ArgumentException("Input cannot be null or empty", nameof(input));
+            }
+
+            if (input.Equals("Larry"))
+            {
+                _someInterface.SomeCallAsync("no");
+            }
+            
+            string transformedString = input.ToUpper();
+            await _someInterface.SomeCallAsync(transformedString);
+            return transformedString;
+        }
+    }
+    
+    public interface ISomeInterface
+    {
+        Task SomeCallAsync(string x);
+    }
+}
